6308e288011304d64f3de4331f31c927d89e90ea
Juan Pablo Lasprilla Correa <juanpablolasprillacorrea@gmail.com>
2024-12-28 09:29:43 -0500

    Initial Commit

---
+++ SimpleCosumingJSONApp_MVPattern/SimpleCosumingJSONApp_MVPatternApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleCosumingJSONApp_MVPatternApp.swift
+//  SimpleCosumingJSONApp_MVPattern
+//
+//  Created by Juan Pablo Lasprilla Correa on 28/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleCosumingJSONApp_MVPatternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
